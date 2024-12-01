1650b8b4b44f19d7d8c4ee2570b5fd57d1fea062
creepysta <ssamajpati97@gmail.com>
2024-12-01 14:15:00 +0530

    init scripts

---
+++ 2024/init.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# set -eux
+
+year=2024
+day=$1
+session_token_path="${HOME}/.aoc.session"
+
+
+if [[ -z "$day" ]]; then
+    echo "No argument supplied. Exiting";
+    return 1;
+fi
+
+
+dir_name=day$day
+inp_file="$dir_name/in.txt"
+
+if [[ -d "$dir_name" ]]; then
+    echo "$dir_name already exists.";
+else
+    echo "Initializing $dir_name ..."
+    mkdir $dir_name
+    cp ./.cr.template $dir_name/a.cr
+    sed -i '' -e "s/{DAY_XX}/$dir_name/g" "$dir_name/a.cr"
+fi
+
+
+if [[ -f "$session_token_path" ]]; then
+    if [[ ! -f "$inp_file" ]]; then
+        session=$(cat $session_token_path)
+        curl -H "cookie: $session;" "https://adventofcode.com/$year/day/$(($day))/input" > "$inp_file"
+    fi
+else
+    cat $session_token_path
+    echo "Cannot find session token at $session_token_path. Going to skip downloading input.";
+fi
+
+cd $dir_name
+
+# set +eux
